155038f47cf0c123898127a6bd932ed56b9a2b91
Nikcolas <nicolas.tancredi_wu@ctrl365.com>
2021-06-12 16:54:11 -0300

    avances de la api-  error get and post

---
+++ src/api/api.js
@@ -0,0 +1,75 @@
+const badRequest = require('./badRequest');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const unq = require("../unqfy"); // importamos el modulo unqfy
+const unqfy = new unq.UNQfy();
+const artistDoesNotExistsError = require ('../errores/ArtistDoesNotExistError');
+const duplicateArtist = require('../errores/DuplicateArtistError');
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8000;
+const router = express.Router();
+
+router.get('/' , (req,res) => {
+    res.status(201); //puedo cambiar el status code
+    res.send(JSON.stringify({message: 'welcome to the api'}));
+});
+
+app.use('/', router) ;
+
+app.listen(
+    port,
+    () => console.log('Esto funciona en puerto : '+ port)
+);
+
+
+function valid(data, expectedKeys) {
+    console.log(data);
+    return Object.keys(expectedKeys).every(key => {
+        return (typeof data[key]) === expectedKeys[key];
+    });
+
+}
+
+function checkValidInput(data, expectedKeys, res, next) {
+    if (!valid(data, expectedKeys)) {
+        throw next(new badRequest());
+    }
+}
+
+app.post('/artists', (req, res, next) => {
+
+    // checkValidInput(req.body, { name: 'string', country: 'string' }, res, next);
+
+
+    let artist = null;
+    try {
+        artist = req.unq.addArtist(req.body);
+        unqfy.save();
+        res.status(201).json(artist);
+    } catch (error) {
+        throw next(new duplicateArtist());
+    }
+});
+
+app.get('/artists/:artistId', (req, res, next) => {
+    const artistId = parseInt(req.params.artistId);
+
+    const artist = req.unqfy.getArtistById(artistId);
+
+    if (!artist) {
+        throw next(new artistDoesNotExistsError());
+    }
+
+    res.status(200).json(artist);
+});
+
+app.use((req, res, next) => {
+    req.unqfy = unqfy.getUNQfy();
+    next();
+});
+
+
