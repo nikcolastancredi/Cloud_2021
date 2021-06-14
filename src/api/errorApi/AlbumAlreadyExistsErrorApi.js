0d679bd60218094bd1834adb1bd913b6c01da863
Nikcolas <nicolas.tancredi_wu@ctrl365.com>
2021-06-14 13:42:38 -0300

    delete folder de erroresApi

--- src/api/errorApi/AlbumAlreadyExistsErrorApi.js
+++
@@ -1,9 +0,0 @@
-class AlbumAlreadyExistsErrorApi extends Error {
-    constructor() {
-        super("Duplicate Album Error");
-        this.status = 409;
-        this.errorCode = 'RESOURCE_ALREADY_EXISTS';
-    }
-}
-
-module.exports = AlbumAlreadyExistsErrorApi; 
